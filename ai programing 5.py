7f40075b3eb6da6018e698ef4ab2c75ba0e57b55
jinhay78 <jinhay78@gmail.com>
2024-09-10 21:44:38 +0900

    ai programing 5

--- ai programing 5.py
+++ ai programing 5.py
@@ -6,12 +6,31 @@ def main():
             self.y=number2
         def __lt__(self,anoter):
             return self.x<anoter.x and self.y < anoter.y
+        def __le__(self,anoter):
+            return self.x<=anoter.x and self.y <=anoter.y
+        def __eq__(self,anoter):
+            return self.x==anoter.x and self.y ==anoter.y
+        def __ne__(self,anoter):
+            return self.x!=anoter.x and self.y !=anoter.y
+        def __gt__(self,anoter):
+            return self.x>anoter.x and self.y >anoter.y
+        def __ge__(self,anoter):
+            return self.x>=anoter.x and self.y >=anoter.y
     c1=comparison(4,2)
     c2=comparison(5,8)
     c3=c1 < c2
+    c4=c1<=c2
+    c5=c1==c2
+    c6=c1!=c2
+    c7=c1>c2
+    c8=c1>=c2
     print(c3)
+    print(c4)
+    print(c5)
+    print(c6)
+    print(c7)
+    print(c8)
 main()
  
-            
             
 
