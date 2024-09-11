52a27b19d5f6c258a87e15a3b5fe4614bca4f123
jinhay78 <jinhay78@gmail.com>
2024-09-11 17:16:56 +0900

    ai programing 4

--- ai programing 4.py
+++ ai programing 4.py
@@ -1,19 +1,20 @@
+
 def main():
     class user:
         def __init__(self):
             self.name=[]
-        def append(self,string):
+        def push(self,string):
             self.name.append(string)
         def __getitem__(self,item):
             return self.name[item]
     person=user()
-    for i in range(10):
+    for i in range(1000000):
         a=input()
         if a.isalpha():
-            person.append(a)
+            person.push(a)
         else:
             break
     print(person.name)
     print(person[0])
 main()
-            
+   
