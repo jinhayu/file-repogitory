60154d86efd0e4e38465bb1646fdfad1ee48c962
jinhay78 <jinhay78@gmail.com>
2024-09-15 02:24:03 +0900

    코드업연습 클래스

---
+++ ai programing 12.py
@@ -0,0 +1,7 @@
+def main():
+    class print1:
+        def pirntm(self):
+            print("Hello")
+    a=print1()
+    a.pirntm()
+main()
---
+++ ai programing 13.py
@@ -0,0 +1,8 @@
+def main():
+    class prinm:
+        def print1(self):
+            print("Hello")
+            print("World")
+    a=prinm()
+    a.print1()
+main()
