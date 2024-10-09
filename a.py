83001b8d4998ede9e51d1ba204f2bdf886be824f
jinhay78 <jinhay78@gmail.com>
2024-10-09 19:06:37 +0900

    비트 코딩 문제

---
+++ a.py
@@ -0,0 +1,10 @@
+# 비트 코딩 problom 394 
+def main():
+    class person:
+        def __init__(self,name,workingfor):
+            self.name=name
+            self.workingfor=workingfor
+    a=person("you jin ha","kumoh")
+    print(a.name)
+    print(a.workingfor)
+main()
